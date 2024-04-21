2bf36362d20fb25381e34f9aa22c4f16aaa5819f
mohammedafeef <mohammedafeef.maf@gmail.com>
2024-04-21 21:21:44 +0530

    config: add tailwind to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 function App() {
-
   return (
-    <>
-      <div>
-        Hello world!
-      </div>
-    </>
-  )
+    <div className="bg-slate-800 p-6 text-white text-base">Hello, world</div>
+  );
 }
 
 export default App;
