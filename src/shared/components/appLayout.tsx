25d3d9de3955e2353d319bad05e2f011c82c3423
mohammedafeef <mohammedafeef.maf@gmail.com>
2024-04-22 10:10:21 +0530

    fix: make the source button to conditional one based on source link prop

--- src/shared/components/appLayout.tsx
+++ src/shared/components/appLayout.tsx
@@ -41,10 +41,15 @@ const Breadcrumb: FC<BreadcrumbProps> = ({ links }) => {
 };
 type AppLayoutProps = {
   title: string;
+  sourceCode?: string;
   children: ReactNode;
 };
 
-export const AppLayout: FC<AppLayoutProps> = ({ title, children }) => {
+export const AppLayout: FC<AppLayoutProps> = ({
+  title,
+  sourceCode,
+  children
+}) => {
   return (
     <div className="h-screen w-screen md:p-10 flex flex-col bg-slate-900 text-white p-6">
       <header className="flex justify-between mb-6">
@@ -59,11 +64,12 @@ export const AppLayout: FC<AppLayoutProps> = ({ title, children }) => {
           />
           <h1 className="text-xl md:text-3xl mt-3">{title}</h1>
         </div>
-        {/* TODO: Add github source ref link button */}
         <div className="flex items-end">
-          <button className="bg-white/80 text-slate-900 px-3 py-2 ring-0 ring-gray-400 rounded text-sm hover:bg-white/90">
-            Source code
-          </button>
+          {sourceCode && (
+            <button className="bg-white/80 text-slate-900 px-3 py-2 ring-0 ring-gray-400 rounded text-sm hover:bg-white/90">
+              Source code
+            </button>
+          )}
         </div>
       </header>
       <div className="flex-1">{children}</div>
